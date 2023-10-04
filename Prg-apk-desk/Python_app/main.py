994a5b7be4bd1ddec51559e1df15bbd85a814fd7
arcziosppl <arturmeus230@gmail.com>
2023-10-04 12:26:39 +0200

    Incorrect data warning

--- Prg-apk-desk/Python_app/main.py
+++ Prg-apk-desk/Python_app/main.py
@@ -4,6 +4,8 @@ from geopy.geocoders import Nominatim
 import customtkinter
 from PIL import Image
 import tkintermapview
+from CTkMessagebox import CTkMessagebox
+
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -11,6 +13,7 @@ class App(customtkinter.CTk):
         self.geometry("400x750")
         self.grid_columnconfigure(0, weight=1)
         self._set_appearance_mode("dark")
+
         self.title("Weather APP")
 
         self.entry_var = customtkinter.StringVar()
@@ -38,24 +41,29 @@ class App(customtkinter.CTk):
 
     def get_weather(self):
         loc = Nominatim(user_agent="GetLoc")
-        getLoc = loc.geocode(self.entry_var.get())
+        location = self.entry_var.get()
 
-        self.map_widget.set_position(getLoc.latitude, getLoc.longitude, marker=True)
+        if location is None:
+            getLoc = loc.geocode(location)
 
-        req = requests.get('https://api.open-meteo.com/v1/forecast', {
-            'latitude': getLoc.latitude,
-            'longitude': getLoc.longitude,
-            'current_weather': True,
-            'hourly': 'temperature_2m,relativehumidity_2m,windspeed_10m'
+            self.map_widget.set_position(getLoc.latitude, getLoc.longitude, marker=True)
 
-        })
+            req = requests.get('https://api.open-meteo.com/v1/forecast', {
+                'latitude': getLoc.latitude,
+                'longitude': getLoc.longitude,
+                'current_weather': True,
+                'hourly': 'temperature_2m,relativehumidity_2m,windspeed_10m'
 
-        open('./files/data.json', "w").write(req.text)
-        data = json.loads(req.text)["current_weather"]
-        self.temp.set(f'{data["temperature"]} °C')
-        self.windspeed.set(f'{data["windspeed"]} Km/h')
-        self.winddirection.set(f'{data["winddirection"]} °')
-        self.weathercode.set(f'{data["weathercode"]} WMO')
+            })
+
+            open('./files/data.json', "w").write(req.text)
+            data = json.loads(req.text)["current_weather"]
+            self.temp.set(f'{data["temperature"]} °C')
+            self.windspeed.set(f'{data["windspeed"]} Km/h')
+            self.winddirection.set(f'{data["winddirection"]} °')
+            self.weathercode.set(f'{data["weathercode"]} WMO')
+        else:
+            CTkMessagebox(title="Error", message="Proszę podać nazwę miasta", icon="cancel")
 
 
 app = App()
