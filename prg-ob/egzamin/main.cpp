b6bda330125e02dead0b6d91bd8b98e383e6a5a8
arcziosppl <arturmeus230@gmail.com>
2023-11-01 21:36:21 +0100

    egzamin - zmiana

--- prg-ob/egzamin/main.cpp
+++ prg-ob/egzamin/main.cpp
@@ -1,9 +1,7 @@
 #include "iostream"
 #include "cstdlib"
-#include "array"
 
 using namespace std;
-
 
 class linearSearch{
 private:
@@ -14,7 +12,7 @@ private:
 public:
     void fill_array(){
         srand(time(nullptr));
-        this->array_length = sizeof(array_to_search)/ sizeof(array_to_search[0]);
+        array_length = sizeof(array_to_search) / sizeof(array_to_search[0]);
 
         array_to_search[array_length-1] = search_number;
         for(int i=0; i<array_length; i++){
@@ -23,13 +21,13 @@ public:
     }
 
     void get_search_number(){
-        cin>>this->search_number;
+        cin>>search_number;
     }
 
     int search_array(){
         int index = 0;
-        for (int i = 0; i <this->array_length; ++i) {
-            if(this->array_to_search[i] == this->search_number){
+        for (int i = 0; i <array_length; ++i) {
+            if(array_to_search[i] == search_number){
                 index = i;
                 break;
             }
@@ -40,8 +38,12 @@ public:
 
     void print_array() const{
         cout<<"Szukana tablica: [";
-        for(int i=0; i< this->array_length; i++){
-            cout<< this->array_to_search[i]<<",";
+        for(int i=0; i< array_length; i++){
+            cout<< array_to_search[i];
+            if(i == array_length-1){
+                break;
+            }
+            cout<<",";
         }
         cout<<"]";
         cout<<"\n";
@@ -59,6 +61,6 @@ int main(){
     if(search.search_array() == 0){
         cout<<"Nie znaleziono podanego elementu";
     } else{
-        cout<<"Znaleziono podany element pod indexem: "<<search.search_array();
+        cout<<"Znaleziono podany element pod indexem: ["<<search.search_array()<<"]";
     }
 }
